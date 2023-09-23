59bcacfcf97568d6f15db9e7c6eb136d8a986a77
Baraa Mohamed <albaraa.m.m2005@gmail.com>
2023-09-23 08:44:20 +0300

    cart change

--- js/script.js
+++ js/script.js
@@ -5,14 +5,15 @@ if (JSON.parse(localStorage.getItem("cart"))) {
   console.log();
 }
 
-console.log(cart);
-
 
 async function getProducts() {
   let response = await axios.get('https://fakestoreapi.com/products');
   let products = response.data;
 
   document.getElementById("products").innerHTML = "";
+  document.querySelector(".cart").innerHTML = `
+    <button class="cart-btn" onClick="openCart()">Cart</button>
+  `;
 
   for (product of products) {
     document.getElementById("products").innerHTML += `
@@ -26,8 +27,9 @@ async function getProducts() {
     `
   }
 }
-
 getProducts();
+
+
 
 async function viewProduct(id) {
   let response = await axios.get('https://fakestoreapi.com/products/' + id);
@@ -46,9 +48,7 @@ async function viewProduct(id) {
 }
 
 
-
 function addProduct(productId, productTitle, productDescription, productPrice, productImage) {
-  // function addProduct(product) {
   let productData = {
     id: productId,
     title: productTitle,
@@ -59,23 +59,40 @@ function addProduct(productId, productTitle, productDescription, productPrice, p
   console.log(cart);
   cart.push(productData);
   localStorage.setItem("cart", JSON.stringify(cart));
+  let popup = `
+    <div class="popup">
+      The product has been added to your cart
+        <button onClick="closePopup()">Ok</button>
+    </div>
+  `
+  document.querySelector(".container").innerHTML += popup;
 }
+
+
+function closePopup() {
+  document.querySelector(".popup").remove();
+}
+
 
 function openCart() {
   let products = cart;
 
+  document.querySelector(".cart").innerHTML += `
+    <button class="return-btn" onClick="getProducts()">Return</button>
+  `;
+
+  document.querySelector(".cart-btn").remove();
 
   document.getElementById("products").innerHTML = "";
 
   for (product of products) {
 
-    document.getElementById("products").innerHTML += `
+    document.getElementById("products").innerHTML = `
     <div class="product">
       <img src="${product.image}" class="product-img">
       <h2 class="product-name">${product.title}</h2>
       <p class="product-description">${product.description}</p>
       <h3 class="product-description">${product.price}</h3>
-      <button onClick="getProducts()">return</button>
     </div>
     `
   }
