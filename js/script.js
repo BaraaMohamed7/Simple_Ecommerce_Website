b6a20650a3c83d7c9dd8072e53fd6341f3c06df5
Baraa Mohamed <albaraa.m.m2005@gmail.com>
2023-09-25 00:39:22 +0300

    added categorization and loading

--- js/script.js
+++ js/script.js
@@ -5,14 +5,59 @@ if (JSON.parse(localStorage.getItem("cart"))) {
   console.log();
 }
 
+async function getCategories() {
+  let response = await axios.get("https://fakestoreapi.com/products/categories");
+  let categories = response.data;
+
+  for (let category of categories) {
+    document.getElementById("categories").innerHTML += `
+      <li>
+        <button class ="category">
+          ${category}
+        </button>
+      </li>
+    `
+  }
+
+  categoriesList = document.querySelectorAll(".category");
+
+  categoriesList.forEach(categoryBtn => {
+    categoryBtn.addEventListener('click',
+      function () {
+        categoriesList.forEach(categoryBtn => {
+          categoryBtn.classList.remove("active")
+        });
+
+
+        categoryBtn.classList.add("active");
+        getProductsCategrized(categoryBtn.innerHTML);
+      }
+    );
+  });
+}
 
 async function getProducts() {
+  loading()
   let response = await axios.get('https://fakestoreapi.com/products');
   let products = response.data;
 
   document.getElementById("products").innerHTML = "";
   document.querySelector(".cart").innerHTML = `
-    <button class="cart-btn" onClick="openCart()">Cart</button>
+    <button class="cart-btn" onClick="openCart()">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="16"
+									height="16"
+									fill="currentColor"
+									class="bi bi-cart"
+									viewBox="0 0 16 16"
+								>
+									<path
+										d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"
+									/>
+								</svg>
+								<div class="cart-amount">${cart.length}</div>
+							</button>
   `;
 
   for (product of products) {
@@ -30,11 +75,53 @@ async function getProducts() {
     document.getElementById("products").innerHTML += productElement;
   }
 }
-getProducts();
+async function getProductsCategrized(category) {
+  loading()
+  let response = await axios.get('https://fakestoreapi.com/products/category/' + category.trim());
+  let products = response.data;
+  console.log(category);
+
+  document.getElementById("products").innerHTML = "";
+  document.querySelector(".cart").innerHTML = `
+    <button class="cart-btn" onClick="openCart()">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="16"
+									height="16"
+									fill="currentColor"
+									class="bi bi-cart"
+									viewBox="0 0 16 16"
+								>
+									<path
+										d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"
+									/>
+								</svg>
+								<div class="cart-amount">${cart.length}</div>
+							</button>
+  `;
+
+  for (product of products) {
+    let productElement = `
+    <div class="product">
+      <div class="product-image">
+        <img src="${product.image}">
+      </div>
+      <h2 class="product-name">${product.title}</h2>
+      <p class="product-description">${product.description}</p>
+      <h3 class="product-price">${product.price}</h3>
+      <button onClick="viewProduct(${product.id})">View</button>
+    </div>
+    `
+    document.getElementById("products").innerHTML += productElement;
+  }
+}
+getProducts("");
+getCategories();
 
 
 
 async function viewProduct(id) {
+  loading()
   let response = await axios.get('https://fakestoreapi.com/products/' + id);
   let product = response.data;
 
@@ -79,6 +166,7 @@ function closePopup() {
 
 
 function openCart() {
+  loading()
   let products = cart;
 
   document.querySelector(".cart").innerHTML += `
@@ -106,4 +194,13 @@ function openCart() {
 
 function search() {
 
+}
+
+function loading() {
+  document.getElementById("products").innerHTML = `
+    <div class="loading">
+      <div>
+      </div>
+    </div>
+  `
 }
