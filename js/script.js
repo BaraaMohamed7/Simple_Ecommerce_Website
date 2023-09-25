90757d25519519c45baa895d895075bafbb3eee0
Baraa Mohamed <albaraa.m.m2005@gmail.com>
2023-09-25 07:12:53 +0300

    added search function

--- js/script.js
+++ js/script.js
@@ -79,7 +79,6 @@ async function getProductsCategrized(category) {
   loading()
   let response = await axios.get('https://fakestoreapi.com/products/category/' + category.trim());
   let products = response.data;
-  console.log(category);
 
   document.getElementById("products").innerHTML = "";
   document.querySelector(".cart").innerHTML = `
@@ -115,8 +114,7 @@ async function getProductsCategrized(category) {
     document.getElementById("products").innerHTML += productElement;
   }
 }
-getProducts("");
-getCategories();
+
 
 
 
@@ -147,7 +145,6 @@ function addProduct(productId, productTitle, productDescription, productPrice, p
     price: productPrice,
     image: productImage,
   }
-  console.log(cart);
   cart.push(productData);
   localStorage.setItem("cart", JSON.stringify(cart));
   let popup = `
@@ -156,6 +153,25 @@ function addProduct(productId, productTitle, productDescription, productPrice, p
         <button onClick="closePopup()">Ok</button>
     </div>
   `
+
+  document.querySelector(".cart").innerHTML = `
+    <button class="cart-btn" onClick="openCart()">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="16"
+									height="16"
+									fill="currentColor"
+									class="bi bi-cart"
+									viewBox="0 0 16 16"
+								>
+									<path
+										d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"
+									/>
+								</svg>
+								<div class="cart-amount">${cart.length}</div>
+							</button>
+              <button class="return-btn" onClick="getProducts()">Return</button>
+  `;
   document.querySelector(".container").innerHTML += popup;
 }
 
@@ -192,9 +208,30 @@ function openCart() {
   }
 }
 
-function search() {
+async function search() {
+  await getProducts();
+  let searchBar = document.getElementById("searchBar").value.toLowerCase();
+  if (searchBar == "") return;
 
+  let matches = Array.from(
+    document.querySelectorAll(".product")
+  ).filter(product => product.textContent.toLowerCase().includes(searchBar))
+
+  if (matches.length == 0) {
+    document.getElementById("products").innerHTML = `
+    <h3> No results </h3>
+    `;
+    return;
+  }
+
+  document.getElementById("products").innerHTML = "";
+  for (let match of matches) {
+    match.classList.add("product-view");
+    console.log(match.textContent);
+    document.getElementById("products").append(match);
+  }
 }
+
 
 function loading() {
   document.getElementById("products").innerHTML = `
@@ -204,3 +241,12 @@ function loading() {
     </div>
   `
 }
+
+
+getProducts("");
+getCategories();
+
+document.getElementById("searchBtn").addEventListener('click', function (e) {
+  e.preventDefault();
+  search();
+})
