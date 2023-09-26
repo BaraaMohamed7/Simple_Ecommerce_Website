b5b3cbbe320b84ad575afdbea953412fb1625f2f
Baraa Mohamed <albaraa.m.m2005@gmail.com>
2023-09-26 12:57:27 +0300

    added loading screen

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import parseRequestUrl from './utils.js';
 import Error404Screen from './routes/error404route.js'
 import CartRoute from './routes/cartRoute.js';
 import Search from './routes/Search.js';
+import { getCartItems } from './localStorage.js';
 
 const routes = {
   '/': Home,
@@ -21,7 +22,14 @@ const router = async () => {
   const route = routes[parseUrl] ? routes[parseUrl] : Error404Screen;
 
   const main = document.getElementById('main-container');
+  main.innerHTML = `
+    <div class="loading">
+      <div>
+      </div>
+    </div>
+  `;
   main.innerHTML = await route.render();
+  document.querySelector('.cart-amount').innerHTML = getCartItems() ? getCartItems().length : 0;
   route.hasOwnProperty('after_render') ? await route.after_render() : null;
 };
 window.addEventListener('load', router);
